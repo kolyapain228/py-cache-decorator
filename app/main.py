067e642d7a895f2c0c3be5da74f6bd2d522095bb
Mykola Mosiienko <koljamos@gmail.com>
2025-11-20 17:54:14 +0000

    Created cache deco.

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def wrapper(*args) -> None:
+        if args not in cache_dict:
+            cache_dict[args] = func(*args)
+            print("Calculating new result")
+            return cache_dict[args]
+        elif args in cache_dict:
+            print("Getting from cache")
+            return cache_dict[args]
+    return wrapper
