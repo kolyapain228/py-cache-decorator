e9d037308ed1a1b5b6b5709e68dea199fac69d83
Mykola Mosiienko <koljamos@gmail.com>
2025-11-20 18:13:51 +0000

    Created cache deco. Refactored return type and logic.

--- app/main.py
+++ app/main.py
@@ -5,10 +5,14 @@ def cache(func: Callable) -> Callable:
     cache_dict = {}
 
     def wrapper(*args) -> Callable:
+
         if args not in cache_dict:
             cache_dict[args] = func(*args)
             print("Calculating new result")
+
         elif args in cache_dict:
             print("Getting from cache")
+
         return cache_dict[args]
+
     return wrapper
